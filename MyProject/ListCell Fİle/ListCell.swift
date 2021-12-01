397a6a996633cbe40a4b3423a81ec74c6433802b
Onuralp <xonuraralpx@gmail.com>
2021-12-01 23:31:55 +0300

    Changed

---
+++ MyProject/ListCell Fİle/ListCell.swift
@@ -0,0 +1,30 @@
+//
+//  ListCell.swift
+//  MyProject
+//
+//  Created by Ceren Çapar on 16.11.2021.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var albumNamesLabelField: UILabel!
+    
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        fixLabelField()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
+extension ListCell{
+    fileprivate func fixLabelField() {
+        albumNamesLabelField.lineBreakMode = .byWordWrapping
+        albumNamesLabelField.numberOfLines = 0
+    }
+}
